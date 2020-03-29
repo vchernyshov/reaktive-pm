41b74fa744366797f3560642262aacdf2364926c
v.chernyshov <morkovkacorp@gmail.com>
2020-03-29 19:26:54 +0300

    Comment ios implementation

--- rpm/src/iosMain/kotlin/dev/garage/rpm/base/PmUiViewController.kt
+++ rpm/src/iosMain/kotlin/dev/garage/rpm/base/PmUiViewController.kt
@@ -1,42 +1,42 @@
 package dev.garage.rpm.base
 
-import dev.garage.rpm.PmView
-import dev.garage.rpm.PresentationModel
-import dev.garage.rpm.delegate.PmUiViewControllerDelegate
-import platform.UIKit.UIViewController
-
-abstract class PmUiViewController<PM : PresentationModel> : UIViewController(), PmView<PM> {
-
-    private val delegate by lazy(LazyThreadSafetyMode.NONE) {
-        PmUiViewControllerDelegate(
-            this,
-            PmUiViewControllerDelegate.RetainMode.CONFIGURATION_CHANGES
-        )
-    }
-
-    final override val presentationModel get() = delegate.presentationModel
-
-    override fun viewDidLoad() {
-        super.viewDidLoad()
-        delegate.onCreate()
-    }
-
-    override fun viewWillAppear(animated: Boolean) {
-        super.viewWillAppear(animated)
-        delegate.onPostCreate()
-        delegate.onStart()
-        delegate.onResume()
-    }
-
-    override fun viewWillDisappear(animated: Boolean) {
-        super.viewWillDisappear(animated)
-        delegate.onPause()
-        delegate.onStop()
-
-    }
-
-    override fun viewDidUnload() {
-        super.viewDidUnload()
-        delegate.onDestroy()
-    }
-}
+//import dev.garage.rpm.PmView
+//import dev.garage.rpm.PresentationModel
+//import dev.garage.rpm.delegate.PmUiViewControllerDelegate
+//import platform.UIKit.UIViewController
+//
+//abstract class PmUiViewController<PM : PresentationModel> : UIViewController(), PmView<PM> {
+//
+//    private val delegate by lazy(LazyThreadSafetyMode.NONE) {
+//        PmUiViewControllerDelegate(
+//            this,
+//            PmUiViewControllerDelegate.RetainMode.CONFIGURATION_CHANGES
+//        )
+//    }
+//
+//    final override val presentationModel get() = delegate.presentationModel
+//
+//    override fun viewDidLoad() {
+//        super.viewDidLoad()
+//        delegate.onCreate()
+//    }
+//
+//    override fun viewWillAppear(animated: Boolean) {
+//        super.viewWillAppear(animated)
+//        delegate.onPostCreate()
+//        delegate.onStart()
+//        delegate.onResume()
+//    }
+//
+//    override fun viewWillDisappear(animated: Boolean) {
+//        super.viewWillDisappear(animated)
+//        delegate.onPause()
+//        delegate.onStop()
+//
+//    }
+//
+//    override fun viewDidUnload() {
+//        super.viewDidUnload()
+//        delegate.onDestroy()
+//    }
+//}
--- rpm/src/iosMain/kotlin/dev/garage/rpm/delegate/PmUiViewControllerDelegate.kt
+++ rpm/src/iosMain/kotlin/dev/garage/rpm/delegate/PmUiViewControllerDelegate.kt
@@ -5,73 +5,71 @@ import dev.garage.rpm.PresentationModel
 import dev.garage.rpm.navigation.UiViewControllerNavigationMessageDispatcher
 import platform.UIKit.UIViewController
 
-class PmUiViewControllerDelegate<PM, F>(
-    private val pmUiViewController: F,
-    private val retainMode: RetainMode
-)
-        where PM : PresentationModel,
-              F : UIViewController, F : PmView<PM> {
-
-    /**
-     * Strategies for retaining the PresentationModel[PresentationModel].
-     * [SAVED_STATE] - the PresentationModel will be destroyed if the Activity is finishing or the Fragment state has not been saved.
-     * [CONFIGURATION_CHANGES] - Retain the PresentationModel during a configuration change.
-     */
-    enum class RetainMode { SAVED_STATE, CONFIGURATION_CHANGES }
-
-    private val commonDelegate = CommonDelegate<PM, F>(pmUiViewController,
-        UiViewControllerNavigationMessageDispatcher(pmUiViewController)
-    )
-
-    val presentationModel: PM get() = commonDelegate.presentationModel
-
-    /**
-     * You must call this method from the containing [Activity]'s corresponding method.
-     */
-    fun onCreate() {
-        commonDelegate.onCreate()
-    }
-
-    /**
-     * You must call this method from the containing [Activity]'s corresponding method.
-     */
-    fun onPostCreate() {
-        commonDelegate.onBind()
-    }
-
-    /**
-     * You must call this method from the containing [Activity]'s corresponding method.
-     */
-    fun onStart() {
-        // For symmetry, may be used in the future
-    }
-
-    /**
-     * You must call this method from the containing [Activity]'s corresponding method.
-     */
-    fun onResume() {
-        commonDelegate.onResume()
-    }
-
-    /**
-     * You must call this method from the containing [Activity]'s corresponding method.
-     */
-    fun onPause() {
-        commonDelegate.onPause()
-    }
-
-    /**
-     * You must call this method from the containing [Activity]'s corresponding method.
-     */
-    fun onStop() {
-        // For symmetry, may be used in the future
-    }
-
-    /**
-     * You must call this method from the containing [Activity]'s corresponding method.
-     */
-    fun onDestroy() {
-        commonDelegate.onUnbind()
-        commonDelegate.onDestroy()
-    }
-}
+//class PmUiViewControllerDelegate<PM: PresentationModel>(
+//    private val pmUiViewController: PmUiViewController<PM>,
+//    private val retainMode: RetainMode
+//) {
+//
+//    /**
+//     * Strategies for retaining the PresentationModel[PresentationModel].
+//     * [SAVED_STATE] - the PresentationModel will be destroyed if the Activity is finishing or the Fragment state has not been saved.
+//     * [CONFIGURATION_CHANGES] - Retain the PresentationModel during a configuration change.
+//     */
+//    enum class RetainMode { SAVED_STATE, CONFIGURATION_CHANGES }
+//
+//    private val commonDelegate = CommonDelegate(pmUiViewController,
+//        UiViewControllerNavigationMessageDispatcher(pmUiViewController)
+//    )
+//
+//    val presentationModel: PM get() = commonDelegate.presentationModel
+//
+//    /**
+//     * You must call this method from the containing [Activity]'s corresponding method.
+//     */
+//    fun onCreate() {
+//        commonDelegate.onCreate()
+//    }
+//
+//    /**
+//     * You must call this method from the containing [Activity]'s corresponding method.
+//     */
+//    fun onPostCreate() {
+//        commonDelegate.onBind()
+//    }
+//
+//    /**
+//     * You must call this method from the containing [Activity]'s corresponding method.
+//     */
+//    fun onStart() {
+//        // For symmetry, may be used in the future
+//    }
+//
+//    /**
+//     * You must call this method from the containing [Activity]'s corresponding method.
+//     */
+//    fun onResume() {
+//        commonDelegate.onResume()
+//    }
+//
+//    /**
+//     * You must call this method from the containing [Activity]'s corresponding method.
+//     */
+//    fun onPause() {
+//        commonDelegate.onPause()
+//    }
+//
+//    /**
+//     * You must call this method from the containing [Activity]'s corresponding method.
+//     */
+//    fun onStop() {
+//        // For symmetry, may be used in the future
+//    }
+//
+//    /**
+//     * You must call this method from the containing [Activity]'s corresponding method.
+//     */
+//    fun onDestroy() {
+//        commonDelegate.onUnbind()
+//        commonDelegate.onDestroy()
+//    }
+//}
