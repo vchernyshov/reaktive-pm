293052f4dbefe8171cd43201f7e47ff66097c5e0
v.chernyshov <morkovkacorp@gmail.com>
2020-04-05 23:40:07 +0300

    Add binding of CheckControl to UISwitch

--- rpm/src/iosMain/kotlin/dev/garage/rpm/widget/CheckControlExtensions.kt
+++ rpm/src/iosMain/kotlin/dev/garage/rpm/widget/CheckControlExtensions.kt
@@ -1,2 +1,25 @@
 package dev.garage.rpm.widget
 
+import com.badoo.reaktive.observable.filter
+import com.badoo.reaktive.observable.skip
+import com.badoo.reaktive.utils.atomic.AtomicReference
+import dev.garage.rpm.bindTo
+import dev.garage.rpm.bindings.switchChanges
+import platform.UIKit.UISwitch
+
+fun CheckControl.bindTo(switch: UISwitch) {
+
+    val editing = AtomicReference(false)
+
+    checked.bindTo {
+        editing.value = true
+        switch.on = it
+        editing.value = false
+    }
+
+    switch.switchChanges()
+        .skip(1)
+        .filter { !editing.value && it != checked.value }
+        .bindTo(checkedChanges)
+}
+
