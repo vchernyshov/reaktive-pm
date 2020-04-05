4d27f8201293d0c90c926f71f4a9a60e8fa3319c
v.chernyshov <morkovkacorp@gmail.com>
2020-04-05 22:38:54 +0300

    Fix concurrency issues on iOS

--- app-common/src/commonMain/kotlin/dev/garage/rpm/app/common/main/api/ServerApiSimulator.kt
+++ app-common/src/commonMain/kotlin/dev/garage/rpm/app/common/main/api/ServerApiSimulator.kt
@@ -6,10 +6,11 @@ import com.badoo.reaktive.completable.doOnBeforeComplete
 import com.badoo.reaktive.single.Single
 import com.badoo.reaktive.single.doOnBeforeSuccess
 import com.badoo.reaktive.single.singleOf
+import com.badoo.reaktive.utils.atomic.AtomicReference
+import com.badoo.reaktive.utils.freeze
 import dev.garage.rpm.app.common.notification.Notification
 import dev.garage.rpm.app.common.notification.NotificationManager
 import dev.garage.rpm.delay
-import dev.garage.rpm.util.currentTimeMillis
 import dev.garage.rpm.util.generateRandomUUID
 import kotlin.random.Random
 
@@ -22,24 +23,23 @@ class ServerApiSimulator(
         private const val NOTIFICATION_ID = 112
     }
 
-    private var phone: String? = null
-    private var code: String? = null
-    private val random = Random(currentTimeMillis())
+    private val phone = AtomicReference<String?>(null)
+    private val code = AtomicReference<String?>(null)
 
     override fun sendPhone(phone: String): Completable {
         return completableOfEmpty()
             .delay(DELAY_IN_SECONDS)
             .doOnBeforeComplete {
                 maybeServerError()
-                this.phone = phone
-                code = generateRandomCode().toString()
+                this.phone.value = phone
+                code.value = generateRandomCode().toString()
                 val notification =
                     Notification(
                         id = NOTIFICATION_ID,
                         title = "ReaktivePM Sample",
-                        message = "Confirmation code $code"
+                        message = "Confirmation code ${code.value}"
                     )
-                notificationManager.showNotification(notification)
+                notificationManager.showNotification(notification.freeze())
             }
     }
 
@@ -47,7 +47,7 @@ class ServerApiSimulator(
         return singleOf(TokenResponse(generateRandomUUID()))
             .delay(DELAY_IN_SECONDS)
             .doOnBeforeSuccess {
-                if (this.code != code) {
+                if (this.code.value != code) {
                     throw WrongConfirmationCode("Wrong confirmation code")
                 } else {
                     maybeServerError()
@@ -63,19 +63,19 @@ class ServerApiSimulator(
             .delay(DELAY_IN_SECONDS)
             .doOnBeforeComplete {
                 maybeServerError()
-                phone = null
-                code = null
+                phone.value = null
+                code.value = null
             }
     }
 
     private fun maybeServerError() {
-        if (random.nextInt(100) >= 80) {
+        if (Random.nextInt(100) >= 80) {
             throw ServerError("Service is unavailable. Please try again.")
         }
     }
 
     private fun generateRandomCode(): Int {
-        var c = random.nextInt(10_000)
+        var c = Random.nextInt(10_000)
         if (c < 1000) {
             c = generateRandomCode()
         }
