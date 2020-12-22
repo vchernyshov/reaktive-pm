65467e6ff5d1e11bf24528401a04805a4b2e22be
Arsen Esatov <a.esatov@garage-dev.com>
2020-12-23 00:34:04 +0300

    Fix build process

---
+++ rpm-media/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.multiplatform")
+    id("dev.icerock.mobile.multiplatform")
+    id("maven-publish")
+}
+
+group = "dev.garage.kmp"
+version = Versions.rpm
+
+android {
+    compileSdkVersion(Versions.Android.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(Versions.Android.minSdk)
+        targetSdkVersion(Versions.Android.targetSdk)
+    }
+}
+
+dependencies {
+    mppModule(Deps.Libs.MultiPlatform.rpm)
+    mppModule(Deps.Libs.MultiPlatform.rpmBitmap)
+    mppLibrary(Deps.Libs.MultiPlatform.kotlinStdLib)
+    mppLibrary(Deps.Libs.MultiPlatform.reaktive)
+    mppLibrary(Deps.Libs.MultiPlatform.reaktiveUtils)
+    mppLibrary(Deps.Libs.MultiPlatform.reaktiveAnnotations)
+
+    androidLibrary(Deps.Libs.Android.appCompat)
+    androidLibrary(Deps.Libs.Android.androidExifInterface)
+}
+
+publishing {
+    repositories.maven("https://api.bintray.com/maven/garage-dev/kmp/reaktive-pm/;publish=1") {
+        name = "bintray"
+
+        credentials {
+            username = System.getProperty("BINTRAY_USER")
+            password = System.getProperty("BINTRAY_KEY")
+        }
+    }
+}
